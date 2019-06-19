72e1d29c374022041da1876f3a9990ee88cfb8d4
irving199627 <irving_rosado01@hotmail.com>
2019-06-19 09:48:25 -0500

    base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/share/navbar/navbar.component';
+import { FooterComponent } from './components/share/footer/footer.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule
